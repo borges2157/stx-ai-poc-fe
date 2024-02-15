ab1b2d9940cd8bcbc8410764bdb5af1459ea0302
Felipe Borges <felipe.borges@encora.com>
2024-02-15 17:49:34 -0300

    adding png; changing StarlingX to WNDRVR

--- chatbot.py
+++ chatbot.py
@@ -27,7 +27,7 @@ def submit(model, temperature, host_ip):
 host_ip = os.environ['HOST_IP']
 host_port = os.environ['HOST_PORT']
 
-CHATBOT_AVATAR_ADDRESS = f'http://{host_ip}:{host_port}/console/static/images/wr-studio-logo-black.png'
+CHATBOT_AVATAR_ADDRESS = 'wr-studio-logo-black.png'
 
 if "step" not in st.session_state:
     st.session_state["step"] = "create_session"
@@ -43,7 +43,7 @@ if st.session_state.step == "create_session":
 if st.session_state.step == "chat":
     if "messages" not in st.session_state:
         st.session_state["messages"] = [
-            {"role": "assistant", "content": "Hi, I'm your personal StarlingX assistant. How can I help you?"}
+            {"role": "assistant", "content": "Hi, I'm your personal WNDRVR assistant. How can I help you?"}
         ]
 
     for msg in st.session_state.messages:
